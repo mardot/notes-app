83a8c5639280688dc23c81a574a3fde5e4bca98c
mitchell-elliott <mitchell_elliott@live.com.au>
2021-07-11 15:26:18 +1000

    add middleware

---
+++ client/src/components/Notification.js
@@ -0,0 +1,27 @@
+const Notification = ({ message }) => {
+  const errorStyle = {
+    color: "red",
+    background: "lightgrey",
+    fontSize: "20px",
+    borderStyle: "solid",
+    borderRadius: "5px",
+    padding: "10px",
+    marginBottom: "10px",
+  };
+  // const notErrorStyle = {
+  //   color: "green",
+  //   background: "lightgrey",
+  //   fontSize: "20px",
+  //   borderStyle: "solid",
+  //   borderRadius: "5px",
+  //   padding: "10px",
+  //   marginBottom: "10px",
+  // };
+  if (message === null) {
+    return null;
+  }
+
+  return <div style={errorStyle}>{message}</div>;
+};
+
+export default Notification;
---
+++ server/controllers/notes.js
@@ -0,0 +1,30 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/notes");
+
+notesRouter.get("/", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+notesRouter.get("/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.post("/", (request, response, next) => {
+  const body = request.body;
+
+  const note = new Note({
+    content: body.content,
+  });
+});
+
+module.exports = notesRouter;
